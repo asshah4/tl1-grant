d701cb4a856738f0b056bfef2c5cf6f7daec5d4c
asshah4 <mrshahman@gmail.com>
2019-10-07 21:18:09 -0400

    home 3 for biostats

--- mscr/advanced-data-mgmt/0_libraries.R
+++ mscr/advanced-data-mgmt/0_libraries.R
@@ -8,42 +8,22 @@ pacman::p_load(
 
 # R class
 swirl,
+admswirl,
 devtools,
 
 # Development and production
 rmarkdown,
 knitr,
-tinytex,
-kableExtra,
-citr,
-tufte,
 
 # Visualization
-scales,
-ggthemes,
-reshape2,
-gridExtra,
-ggridges,
 RColorBrewer,
 viridis,
-GGally,
-DiagrammeR,
-semPlot,
 
 # Statistical tools
 Hmisc,
 psych,
 lme4,
 nlme,
-lattice,
-rms,
-boot,
-survival,
-survminer,
-tables,
-compareGroups,
-Epi,
-rcompanion,
 
 # Tidying and cleaning data
 magrittr,
